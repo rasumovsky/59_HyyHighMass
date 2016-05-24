df4b65c941cbba53cd071763a833d58f15947d11
Andrew Hard <hardandrew1@gmail.com>
2016-05-24 13:03:20 +0200

    Compiling script

---
+++ scripts/makeAll.sh
@@ -0,0 +1,9 @@
+echo "Compiling all executables for HMMaster"
+make bin/HMMaster  
+make bin/GlobalP0Toys_Faster  
+make bin/GlobalP0Analysis  
+make bin/GenericToys  
+make bin/MassAnimation
+make bin/LocalP0Analysis  
+make bin/TossToys_NoFit
+make bin/AddDataToWorkspace
